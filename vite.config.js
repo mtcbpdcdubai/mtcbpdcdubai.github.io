c2e0894572b070e25b1ff9b7e50b77cb2eeef39c
Sreenikethan I <29738979+SreenikethanI@users.noreply.github.com>
2025-01-15 21:44:14 +0400

    Prepare content directory

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/mtcbpdc',
-})
 
+  // https://medium.com/@pushplaybang/absolutely-dont-use-relative-paths-imports-in-your-vite-react-project-c8593f93bbea
+  // Effectively allows us to import from "content/..."
+  resolve: {
+    alias: {
+      content: "/content"
+    }
+  }
+})
