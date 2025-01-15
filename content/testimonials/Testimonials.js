ee91d6e4e2682cffb9bba1522ae428aeea16d59c
Sreenikethan I <29738979+SreenikethanI@users.noreply.github.com>
2025-01-15 21:50:02 +0400

    Move testimonials to content/ folder

---
+++ content/testimonials/Testimonials.js
@@ -0,0 +1,35 @@
+import imgAmeiya from "./Testimonial_Ameiya.jpg";
+import imgSiddhi from "./Testimonial_Siddhi.jpg";
+import imgVania from "./Testimonial_Vania.jpg";
+
+/** A type declaration for a single Testimonial entry.
+ * @typedef {Object} Testimonial
+ * @property {str} name
+ * @property {str} year
+ * @property {str} quote
+ * @property {} image
+ */
+
+/** @type {Testimonial[]} */
+const testimonials = [
+  {
+    name: "Ameiya Wankhede",
+    year: "2nd Year",
+    quote: "This past semester, being the Creative Ambassador at MTC has been an incredibly rewarding experience. It has offered various opportunities for personal growth and skill development. I was able to understand the functioning of the club and collaborate with an amazing team. All the council members were always so supportive and motivating, that working on all the assigned tasks was genuinely enjoyable. Overall, this program has broadened my horizons and bolstered my confidence and I'd highly recommend this program for anyone looking to elevate themselves personally and professionally. ",
+    image: imgAmeiya,
+  },
+  {
+    name: "Siddhi Mishra",
+    year: "2rd Year",
+    quote: "I am thrilled to share my experience as a proud member of the Ambassadorship Program of MTC. This opportunity has been an incredible journey filled with many valuable learning experiences, growth and fun moments with the council. I can positively say that the members of the council guided me throughout my shortcomings and profoundly impacted my personal and professional development. I would definitely recommend freshers to join the Ambassadorship Program. ",
+    image: imgSiddhi,
+  },
+  {
+    name: "Vania Roy",
+    year: "2rd Year",
+    quote: "The MTC ambassadorship programme helped me improve my technical skills & gain practical experience and the confidence I needed by being involved in various events of the club. I got to learn & assist in the development of coding questions & programming puzzles for many different tasks & competitions too.",
+    image: imgVania,
+  },
+];
+
+export default testimonials;
--- src/components/Testimonials/TestimonialSlider.jsx
+++ src/components/Testimonials/TestimonialSlider.jsx
@@ -1,36 +1,15 @@
 import React, { useState } from "react";
+import testimonials from "content/testimonials/Testimonials";
 import "./TestimonialSlider.css";
-import marketingMember7 from "../../assets/marketing_member7.jpg";
-import Testimonial02 from "../../assets/Testimonial02.jpeg";
-import Testimonial03 from "../../assets/Testimonial03.jpg";
 
 
 const TestimonialSlider = () => {
   const[currentSlide, setCurrentSlide] = useState(0);
-  const testimonials = [
-    {
-      quote: "This past semester, being the Creative Ambassador at MTC has been an incredibly rewarding experience. It has offered various opportunities for personal growth and skill development. I was able to understand the functioning of the club and collaborate with an amazing team. All the council members were always so supportive and motivating, that working on all the assigned tasks was genuinely enjoyable. Overall, this program has broadened my horizons and bolstered my confidence and I'd highly recommend this program for anyone looking to elevate themselves personally and professionally. ",
-      name: "Ameiya Wankhede",
-      year: "2nd Year",
-      image: marketingMember7
-    },
-    {
-      quote: "I am thrilled to share my experience as a proud member of the Ambassadorship Program of MTC. This opportunity has been an incredible journey filled with many valuable learning experiences, growth and fun moments with the council. I can positively say that the members of the council guided me throughout my shortcomings and profoundly impacted my personal and professional development. I would definitely recommend freshers to join the Ambassadorship Program. ",
-      name: "Siddhi Mishra",
-      year: "2rd Year",
-      image:Testimonial02
-    },
-    {
-      quote: "The MTC ambassadorship programme helped me improve my technical skills & gain practical experience and the confidence I needed by being involved in various events of the club. I got to learn & assist in the development of coding questions & programming puzzles for many different tasks & competitions too.",
-      name: "Vania Roy",
-      year: "2rd Year",
-      image:Testimonial03
-    },
-  ];
 
   const nextSlide = () => {
     setCurrentSlide((prev) => (prev + 1) % testimonials.length);
   };
+
   const prvSlide = () => {
     setCurrentSlide(
       (prev) => (prev - 1 + testimonials.length) % testimonials.length
@@ -40,9 +19,11 @@ const TestimonialSlider = () => {
   return (
     <div className="testimonial-slider">
       <h2>Past Ambassador Testimonials</h2>
-      <h4>Don't take our word for it. Take theirs</h4>
+      <h4>Don't take our word for it. Take theirs:</h4>
+
       <div className="testimonial">
-        <button onClick={prvSlide} class="slider-button left">‹</button>
+        <button onClick={prvSlide} className="slider-button left">‹</button>
+
         <div className="testimonial-content">
           <p>"{testimonials[currentSlide].quote}"</p>
           <img
@@ -55,7 +36,8 @@ const TestimonialSlider = () => {
             {testimonials[currentSlide].year}
           </p>
         </div>
-        <button onClick={nextSlide} class="slider-button right">›</button>
+
+        <button onClick={nextSlide} className="slider-button right">›</button>
       </div>
 
       <div className="ondots">
