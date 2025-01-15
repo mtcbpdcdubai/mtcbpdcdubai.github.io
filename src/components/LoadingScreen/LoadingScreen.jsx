1c5808f2df03df023519517b65ea4892bcf0a1fb
Sreenikethan I <29738979+SreenikethanI@users.noreply.github.com>
2025-01-15 16:17:32 +0400

    Spinner now spins forever #1

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -1,35 +1,28 @@
 import React from "react";
-import { motion } from "framer-motion";
 import './LoadingScreen.css';
 
+/** @type {React.CSSProperties} */
 const circleStyle = {
   display: "block",
   width: "5rem",
   height: "5rem",
-  border: "0.5rem solid transparent", 
+  border: "0.5rem solid transparent",
   borderTop: "0.5rem solid #0078D7", // Blue
   borderRight: "0.5rem solid #00BC28", // Green
   borderBottom: "0.5rem solid #D83B01", // Red
   borderLeft: "0.5rem solid #FFB900", // Yellow
   borderRadius: "50%",
   position: "absolute",
-  boxSizing: "border-box"
-};
+  boxSizing: "border-box",
 
-const spinTransition = {
-  loop: Infinity,
-  ease: "linear",
-  duration: 1.5 // Adjust the speed of the spin
+  // spinning animation, defined in the css file
+  animation: "loading-screen-spin 1.5s linear 0s infinite"
 };
 
 export default function LoadingScreen() {
   return (
     <div className="loading-screen">
-      <motion.span
-        style={circleStyle}
-        animate={{ rotate: 360 }}
-        transition={spinTransition}
-      />
+      <span style={circleStyle}/>
     </div>
   );
 }
