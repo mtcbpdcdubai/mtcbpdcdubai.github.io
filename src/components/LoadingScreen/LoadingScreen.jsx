110659148b3a02bd873cd467975a9b8dbf3c89f7
Sreenikethan I <29738979+SreenikethanI@users.noreply.github.com>
2025-01-26 12:49:22 +0400

    Loading screen: moved styles to CSS file

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -1,27 +1,9 @@
 import './LoadingScreen.css';
 
-/** @type {React.CSSProperties} */
-const circleStyle = {
-  display: "block",
-  width: "5rem",
-  height: "5rem",
-  border: "0.5rem solid transparent",
-  borderTop: "0.5rem solid #0078D7", // Blue
-  borderRight: "0.5rem solid #00BC28", // Green
-  borderBottom: "0.5rem solid #D83B01", // Red
-  borderLeft: "0.5rem solid #FFB900", // Yellow
-  borderRadius: "50%",
-  position: "absolute",
-  boxSizing: "border-box",
-
-  // spinning animation, defined in the css file
-  animation: "loading-screen-spin 1.5s linear 0s infinite"
-};
-
 export default function LoadingScreen() {
   return (
     <div className="loading-screen">
-      <span style={circleStyle}/>
+      <span className="circle" />
     </div>
   );
 }
