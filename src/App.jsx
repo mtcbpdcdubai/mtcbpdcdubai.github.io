8a7285317503bdde8ce2e5a188192fa5a17cd987
Sreenikethan I <29738979+SreenikethanI@users.noreply.github.com>
2025-01-29 15:49:08 +0400

    Loading screen now real #1
    Loading screen now waits for the actual code to load, instead of it being hard-coded.

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,32 @@
-import { /*useState, useEffect,*/ lazy, Suspense } from 'react';
+// import { /*useState, useEffect,*/ lazy, Suspense } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import * as Paths from "src/Paths";
 
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import LoadingScreen from './components/LoadingScreen/LoadingScreen';
-const Home = lazy(() => import('./pages/Home/Home'));
-const About = lazy(() => import('./pages/About/About'));
-const Membership = lazy(() => import('./pages/Membership/Membership'));
-const Events = lazy(() => import('./pages/Events/Events'));
-const Contact = lazy(() => import('./pages/Contact/Contact'));
-const Articles = lazy(() => import('./pages/Articles/Articles'));
-const NotFound = lazy(() => import('./pages/NotFound/NotFound'));
+import Navbar     from './components/Navbar/Navbar';
+import Footer     from './components/Footer/Footer';
+import Home       from './pages/Home/Home';
+import About      from './pages/About/About';
+import Membership from './pages/Membership/Membership';
+import Events     from './pages/Events/Events';
+import Contact    from './pages/Contact/Contact';
+import Articles   from './pages/Articles/Articles';
+import NotFound   from './pages/NotFound/NotFound';
 
 
 const App = () => {
-  /*
-  const [isLoading, setIsLoading] = useState(false); // TODO: change to true
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 4000);
-
-    return () => clearTimeout(timer);
-  }, []);
-  */
-
   return (<HashRouter>
-    {/* {isLoading ? <LoadingScreen /> : <> */}
     <Navbar />
-    <Suspense fallback={<LoadingScreen />}>
-      <Routes>
-        <Route exact path={Paths.ROOT} element={<Home />} />
-        <Route exact path={Paths.ABOUT} element={<About />} />
-        <Route exact path={Paths.MEMBERSHIP} element={<Membership />} />
-        <Route exact path={Paths.EVENTS} element={<Events />} />
-        <Route exact path={Paths.CONTACT} element={<Contact />} />
-        <Route exact path={Paths.ARTICLES} element={<Articles />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route exact path={Paths.ROOT} element={<Home />} />
+      <Route exact path={Paths.ABOUT} element={<About />} />
+      <Route exact path={Paths.MEMBERSHIP} element={<Membership />} />
+      <Route exact path={Paths.EVENTS} element={<Events />} />
+      <Route exact path={Paths.CONTACT} element={<Contact />} />
+      <Route exact path={Paths.ARTICLES} element={<Articles />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
     <Footer />
-    {/* </>} */}
   </HashRouter>);
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { lazy, Suspense } from 'react';
+import LoadingScreen from './components/LoadingScreen/LoadingScreen';
+const App = lazy(() => import('./App.jsx'));
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<LoadingScreen />}>
+      <App />
+    </Suspense>
   </StrictMode>,
-)
+);
