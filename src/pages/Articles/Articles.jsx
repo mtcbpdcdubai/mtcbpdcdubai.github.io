f319b555b8024c95a0a286fc2f316ef2e31d78a6
Sreenikethan I <29738979+SreenikethanI@users.noreply.github.com>
2025-01-30 00:22:14 +0400

    Articles page: styling in progress #5

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -7,21 +7,23 @@ import './Articles.css';
 export default function Articles() {
   return (
     <div className="articles-page">
-      <h2>Explore Our Articles</h2>
+      <h1>Explore our Articles</h1>
       <div className="articles-cards">
+
         {articles.map(article => (
-          <div className="article-card card-1" key={article.link}>
+          <div className="article-card" key={article.link}>
             <img src={article.image} alt={article.title} />
             <div className="article-content">
-              <h3>{article.title}</h3>
+              <h2>{article.title}</h2>
               <p>{article.preview}</p>
-              <LinkButton className="read-more-button blue-button" newTab to={article.link}>
+              <LinkButton newTab to={article.link}>
                 <FaMedium />
                 Read More
               </LinkButton>
             </div>
           </div>
         ))}
+
       </div>
     </div>
   );
